58b3ac9da63e384428d215f0b975d2da4337f31d
Mateus Tuono <mateustuono@hotmail.com>
2025-12-05 13:24:13 -0300

    ajuste nfse cidade prest

--- app/Models/NfseServico.php
+++ app/Models/NfseServico.php
@@ -12,10 +12,11 @@ class NfseServico extends Model
     protected $fillable = [
         'nfse_id', 'discriminacao', 'valor_servico', 'servico_id', 'codigo_cnae', 'codigo_tributacao_municipio', 
         'exigibilidade_iss', 'iss_retido', 'data_competencia', 'estado_local_prestacao_servico', 
-        'cidade_local_prestacao_servico', 'valor_deducoes', 'desconto_incondicional', 'desconto_condicional',
+        'valor_deducoes', 'desconto_incondicional', 'desconto_condicional',
         'outras_retencoes', 'aliquota_iss', 'aliquota_pis', 'aliquota_cofins', 'aliquota_inss', 
         'aliquota_ir', 'aliquota_csll', 'intermediador', 'documento_intermediador', 'nome_intermediador', 
-        'im_intermediador', 'codigo_servico', 'responsavel_retencao_iss', 'valor_aliquota', 'aliquota_issqn'
+        'im_intermediador', 'codigo_servico', 'responsavel_retencao_iss', 'valor_aliquota', 'aliquota_issqn',
+        'cidade_local_prestacao_servico_id',
     ];
 
     public function servico(){
